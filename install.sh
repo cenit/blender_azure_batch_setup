bdb83fa911aa1d47ebd254626b744536d90aee27
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-12-18 12:35:38 +0100

    move to the script also the install phase to reduce startuptask length

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update 
+sudo apt-get dist-upgrade
+sudo apt-get -y install blender
+
 blender -b -P enableaddon.py 
-cp /mnt/batch/tasks/apppackages/blender_plugin_pro_lighting_skies_hdris1.02017-12-18-09-43/* /home/_azbatch/.config/blender/2.76/scripts/addons/pro_lighting_skies_demo/hdris/
+cp --verbose /mnt/batch/tasks/apppackages/blender_plugin_pro_lighting_skies_hdris1.02017-12-18-09-43/* /mnt/batch/tasks/startup/wd/.config/blender/2.76/scripts/addons/pro_lighting_skies_demo/hdris/
 
 echo "Installed pro_lighting_skies_demo with hdris resources!"
 echo
+echo Current folder:
+pwd
